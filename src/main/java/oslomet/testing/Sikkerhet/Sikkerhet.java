bdfd04208f4611e5c9f1f9be4d6df0e96828fe9b
solafsen <s374936@oslomet.no>
2024-02-29 14:40:07 +0100

    Fjernet noe fra import

--- src/main/java/oslomet/testing/Sikkerhet/Sikkerhet.java
+++ src/main/java/oslomet/testing/Sikkerhet/Sikkerhet.java
@@ -1,9 +1,6 @@
 package oslomet.testing.Sikkerhet;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import oslomet.testing.DAL.BankRepository;
